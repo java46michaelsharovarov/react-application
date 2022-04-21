64f806878d33a758688773267cb3e71a45d1a136
Mik <m.shac86@gmail.com>
2022-04-21 18:04:30 +0300

    CW_37_tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import lifeGameConfig from './config/lifeGameConfig.json'
+import LifeMatrix from './service/LifeMatrix';
+test('life game nextStep matrix', () => {
+ const ar = new LifeMatrix(lifeGameConfig.matrixTest);
+ const expected = lifeGameConfig.matrixTest;
+ expect(ar.nextStep()).toEqual(expected);
 });
+test('life game nextStep matrix 1', () => {
+  const ar = new LifeMatrix(lifeGameConfig.matrixTest1);
+ const expected = lifeGameConfig.expectedMatrixTest1;
+ expect(ar.nextStep()).toEqual(expected);
+ });
