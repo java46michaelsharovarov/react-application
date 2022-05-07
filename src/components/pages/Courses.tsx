bead8a434f49584326334124bc3103ef691aa1fb
Mik <m.shac86@gmail.com>
2022-05-07 16:13:52 +0300

    HW_40_DataGrid

--- src/components/pages/Courses.tsx
+++ src/components/pages/Courses.tsx
@@ -1,39 +1,29 @@
-import * as React from 'react';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import { Course } from '../../models/Course';
-import { useSelector } from 'react-redux';
-import { StateType } from '../../redux/store';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Course } from "../../models/Course";
+import { StateType } from "../../redux/store"
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const Courses: React.FC = () => {
     const courses: Course[] = useSelector<StateType, Course[]>(state => state.courses);
-    const rows = courses;
-  return (
-    <TableContainer component={Paper} sx={{mt: -3}}>
-      <Table size='small'>
-        <TableHead>
-          <TableRow>
-            <TableCell align="center" style={{fontWeight:'bold'}}>ID</TableCell>
-            <TableCell align="center" style={{fontWeight:'bold'}}>Name</TableCell>
-            <TableCell align="center" style={{fontWeight:'bold'}}>Lecturer</TableCell>
-            <TableCell align="center" style={{fontWeight:'bold'}}>Hours</TableCell>
-            <TableCell align="center" style={{fontWeight:'bold'}}>Cost</TableCell>
-            <TableCell align="center" style={{fontWeight:'bold'}}>Date</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell align="center">{row.id}</TableCell>
-              <TableCell align="center">{row.name}</TableCell>
-              <TableCell align="center">{row.lecturer}</TableCell>
-              <TableCell align="center">{row.hours}</TableCell>
-              <TableCell align="center">{row.cost}</TableCell>
-              <TableCell align="center">{row.openingDate}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', disableColumnMenu: true, flex:1},
+        { field: 'name', headerName: 'Name', disableColumnMenu: true, flex:1},
+        { field: 'lecturer', headerName: 'Lecturer', disableColumnMenu: true, flex:1},
+        { field: 'hours', headerName: 'Hours', type: 'number', disableColumnMenu: true, flex:1},
+        { field: 'cost', headerName: 'Cost', type: 'number', disableColumnMenu: true, flex:1},
+        { field: 'openingDate', headerName: 'Date', disableColumnMenu: true, flex:1}        
+      ];           
+        return (
+          <div style={{width: '100%', marginTop: '-5vw'}}>
+            <DataGrid
+              rows={courses}
+              columns={columns}
+              pageSize={20}
+              autoHeight
+              rowHeight={30}
+            />
+          </div>
+        );
 }
 export default Courses;
